e0c110fff7c31440b5a96b9f9c850b59ab956693
sekhar76405 <ssjio5402@gmail.com>
2021-08-13 17:10:24 +0530

    LAB 1

--- LAB1_CICD/src/LAB1.java
+++ LAB1_CICD/src/LAB1.java
@@ -1,5 +1,6 @@
 public class LAB1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hi this is the first lab of CICD");
+        System.out.println("Test for jenkins build");
     }
 }
